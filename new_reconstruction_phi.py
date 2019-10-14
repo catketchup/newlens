7a4b31edc2d15fc69c53fc9e076bfbd8c78aaa86
Hongbo <hoc34@pitt.edu>
2019-10-14 12:53:13 -0400

    created plot_noise.py

--- new_reconstruction_phi.py
+++ new_reconstruction_phi.py
@@ -1,12 +1,13 @@
-from healpy import sphtfunc
+from healpy import sphtfunc, visufunc
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 import path
 import load_data
 import noise_model
 import ipdb
+import reconstruction_noise
 
-lmax = 3000
+lmax = 1000
 lmin = 2
 ls = np.arange(0, lmax + 1)
 nlev_t = 27.  # telmperature noise level, in uk.arclmin
@@ -26,64 +27,59 @@ class phi_TT:
         """input spectra"""
         self.lmin = args[0]
         self.lmax = args[1]
-        self.zeta = wignerd.gauss_legendre_quadrature(4501)
+        self.nside = args[2]
         self.unlensed_TT = load_data.unlensed(self.lmin, self.lmax,
                                               'TT').spectra()
         self.lensed_TT = load_data.lensed(self.lmin, self.lmax,
                                           'TT').spectra() + nltt
+        self.input_map = sphtfunc.synfast(
+            self.lensed_TT, self.nside)  #create a map from self.lensed_TT
+        self.alm = sphtfunc.map2alm(self.input_map)
 
-    def factor1(self):
+    # def factor1(self):
+    #     ell = np.arange(self.lmin, self.lmax + 1)
+    #     return ell * (ell + 1)
+
+    # def factor2(self):
+    #     ell = np.arange(self.lmin, self.lmax + 1)
+    #     return (2 * ell + 1)**(1 / 2)
+
+    def part1(self):
         ell = np.arange(self.lmin, self.lmax + 1)
-        return ell * (ell + 1)
 
-#    ipdb.set_trace()
+        cl1 = np.sqrt(4 * np.pi) * (-1)**(
+            ell + 1) * self.unlensed_TT[ell] / self.lensed_TT[ell]
+        factor1 = sphtfunc.almxfl(self.alm, cl1)
+        map1 = sphtfunc.alm2map(factor1, self.nside)
 
-    def factor2(self):
-        ell = np.arange(self.lmin, self.lmax + 1)
-        return (2 * ell + 1)**(1 / 2)
+        cl2 = np.sqrt(4 * np.pi) * (-1)**(ell + 1) * (ell * (
+            ell + 1)) / self.lensed_TT[ell]
+        factor2 = sphtfunc.almxfl(self.alm, cl2)
+        map2 = sphtfunc.alm2map(factor2, self.nside)
+
+        phi_lm = sphtfunc.map2alm(map1 * map2)
+
+        phi_cl = (1 / (np.sqrt(4 * np.pi)) * (-1)**
+                  (ell + 1))**2 * sphtfunc.alm2cl(phi_lm)[ell] / (ell *
+                                                                  (ell + 1))**2
+        # visufunc.mollview(map1 * map2)
+        # plt.show()
+        #return reconstruction_noise.TT(self.lmin, self.lmax).noise()
+        return phi_cl
+
+    def part2(self):
+        return 1
 
 
-## alm from a map
-in_powspec = np.arange(2, 1000)
-healpymap = sphtfunc.synfast(in_powspec, 2)
-alm = sphtfunc.map2alm(healpymap)
-#out_powspec = sphtfunc.anafast(healpymap)
-m = np.array([0, 1])
-index = sphtfunc.Alm.getidx(7, 1, m)
-#print(index)
-#print(alm[index])
-#print(out_powspec)
-print(alm)
-
+nside = 400  #nside should be large enough
+test = phi_TT(lmin, lmax, nside)
+phi_cl = np.array(test.part1())
+lmax = 1000
 lmin = 2
-lmax = 3000
-
-
-## test healpy.sphtfunc.almxfl
-def fl1():
-    ell = np.arange(lmin, lmax + 1)
-    return ell * (ell + 1)
-
-
-factor1 = sphtfunc.almxfl(alm, fl1())
-print(factor1)
-
-map1 = sphtfunc.alm2map(factor1, 2)
-print(map1)
-
-
-def fl2():
-    ell = np.arange(lmin, lmax + 1)
-    return (2 * ell + 1)**(1 / 2)
-
-
-factor2 = sphtfunc.almxfl(alm, fl2())
-
-map2 = sphtfunc.alm2map(factor2, 2)
-print(map2)
-
-map = map1 * map2
-print(map)
-
-phi_lm = sphtfunc.map2alm(map)
-print(phi_lm)
+bealm_fwhlm = 7
+nlev_t = 27.  # telmperature noise level, in uk.arclmin
+nlev_p = np.sqrt(2) * 40
+noise_tt = reconstruction_noise.TT(lmin, lmax, bealm_fwhlm, nlev_t,
+                                   nlev_p).noise()
+plt.plot(np.square(noise_tt)[0:999] * phi_cl)
+plt.show()
--- reconstruction_noise.py
+++ reconstruction_noise.py
@@ -6,28 +6,22 @@ import pandas as pd
 import scipy.interpolate
 import load_data
 import noise_model
-# calculate the EB estimator using lmy new result as a delmon
-lmax = 3000
-lmin = 2
-ls = np.arange(0, lmax + 1)
-nlev_t = 27.  # telmperature noise level, in uk.arclmin
-nlev_p = np.sqrt(2) * 40
-bealm_fwhlm = 7
-
-noise = noise_model.noise(bealm_fwhlm, lmax, nlev_t, nlev_p)
-nltt = noise.tt()
-nlee = noise.ee()
-nlbb = noise.bb()
 
 
 class TT:
     def __init__(self, *args):
         self.lmin = args[0]
         self.lmax = args[1]
+        self.bealm_fwhlm = args[2]
+        self.nlev_t = args[3]  # telmperature noise level, in uk.arclmin
+        self.nlev_p = args[4]
+        self.nltt = noise_model.noise(self.bealm_fwhlm, self.lmax, self.nlev_t,
+                                      self.nlev_p).tt()
+
         self.zeta = wignerd.gauss_legendre_quadrature(4501)
         self.array1 = load_data.unlensed(self.lmin, self.lmax, 'TT').spectra()
         self.array2 = load_data.lensed(self.lmin, self.lmax,
-                                       'TT').spectra() + nltt
+                                       'TT').spectra() + self.nltt
 
     def zeta_00(self):
         cl_00 = np.zeros(self.lmax + 1, dtype=complex)
@@ -82,12 +76,21 @@ class EB:
     def __init__(self, *args):
         self.lmin = args[0]
         self.lmax = args[1]
+        self.bealm_fwhlm = args[2]
+        self.nlev_t = args[3]  # telmperature noise level, in uk.arclmin
+        self.nlev_p = args[4]
+        self.nlee = noise_model.noise(self.bealm_fwhlm, self.lmax, self.nlev_t,
+                                      self.nlev_p).ee()
+        self.nlbb = noise_model.noise(self.bealm_fwhlm, self.lmax, self.nlev_t,
+                                      self.nlev_p).bb()
+
         self.zeta = wignerd.gauss_legendre_quadrature(4501)
+
         self.array1 = load_data.unlensed(self.lmin, self.lmax, 'EE').spectra()
         self.array2 = load_data.lensed(self.lmin, self.lmax,
-                                       'EE').spectra() + nlbb
+                                       'EE').spectra() + self.nlee
         self.array3 = load_data.lensed(self.lmin, self.lmax,
-                                       'BB').spectra() + nlbb
+                                       'BB').spectra() + self.nlbb
 
 
 #    import ipdb
@@ -169,57 +172,3 @@ class EB:
 
         ret[ell] = ((np.pi / 4.) * clL[ell])**-1
         return ret
-
-
-def t(ell):
-    return (ell * (ell + 1.))
-
-
-if 0:
-    TT_nl = t(ls) * t(ls) * TT(lmin, lmax).noise() / (2 * np.pi)
-    TT_ql = np.real(np.loadtxt("data/TT.dat", dtype=complex))
-    plt.plot(ls[2:2000], TT_nl[2:2000])
-    plt.plot(ls[2:2000], TT_ql[2:2000])
-    plt.legend(['TT_ql', 'TT_nl'])
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel(r'$L$')
-    plt.ylabel(r'$[L[L+1] C_L^{\phi\phi} / 2\pi$')
-    plt.show()
-    print('done')
-
-if 1:
-    filenalme_TT = './data/hu_TT.csv'
-    filenalme_EB = './data/hu_EB.csv'
-
-    TT_hu = np.array(pd.read_csv(filenalme_TT))
-    EB_hu = np.array(pd.read_csv(filenalme_EB))
-    TT_nl = t(ls) * TT(lmin, lmax).noise() / (2 * np.pi)
-    EB_nl = t(ls) * EB(lmin, lmax).noise() / (2 * np.pi)
-
-# colmpared with Hu's data given by Mat
-if 1:
-    plt.plot(ls[2:2000], TT_nl[2:2000])
-    plt.plot(TT_hu[:, 0], TT_hu[:, 1])
-    plt.legend(['TT_noise', 'TT_noise_hu'])
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel(r'$L$')
-    plt.ylabel(r'$[L[L+1] C_L^{\phi\phi} / 2\pi$')
-    plt.show()
-
-if 1:
-    plt.plot(ls[2:2000], EB_nl[2:2000])
-    plt.plot(EB_hu[:, 0], EB_hu[:, 1])
-    plt.legend(['EB_noise', 'EB_noise_hu'])
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel(r'$L$')
-    plt.ylabel(r'$[L[L+1] C_L^{\phi\phi} / 2\pi$')
-    plt.show()
-
-if 0:
-    f = scipy.interpolate.interp1d(TT_hu[:, 0], TT_hu[:, 1])
-    xnew = np.arange(3, 1500)
-    ynew = f(xnew)
-    print(ynew)
